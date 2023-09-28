2d48d61d3bc4dc5548fb95b7f7ce011b029fe66c
omkar <vyasomkar123@gmail.com>
2023-09-28 12:20:56 +0530

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -36,6 +36,6 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_artifacts()
-    app.run(debug=False,host = "0.0.0.0")
+    app.run(debug=True,host = "0.0.0.0",port = 8080)
 
 
